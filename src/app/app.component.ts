bc7b2989d481a1e9cccfadb56facb3f13ce92c67
kaleb22 <kaleb22@hotmail.com>
2024-11-20 17:31:56 -0300

    feat(home-component): creating home component to display home page and adding routes to it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./shared/header/header.component";
+
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'angular-jornada-milhas';
-}
+export class AppComponent {}
