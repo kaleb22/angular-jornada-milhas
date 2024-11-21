66cac8d507231b9d151ea32c269c73f29e15ba64
kaleb22 <kaleb22@hotmail.com>
2024-11-21 18:18:02 -0300

    feat(card-depoimento-component): implementing style

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { BannerComponent } from '../../shared/banner/banner.component';
 import { ContainerComponent } from '../../shared/container/container.component';
 import { CardBuscaComponent } from '../../shared/card-busca/card-busca.component';
+import { CardDepoimentoComponent } from '../../shared/card-depoimento/card-depoimento.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BannerComponent, ContainerComponent, CardBuscaComponent],
+  imports: [
+    BannerComponent,
+    ContainerComponent,
+    CardBuscaComponent,
+    CardDepoimentoComponent,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
