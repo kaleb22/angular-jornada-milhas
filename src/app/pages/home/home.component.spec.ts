888410590a33abc3b73df76b2e782367ef63f039
kaleb22 <kaleb22@hotmail.com>
2024-11-21 21:26:19 -0300

    fix(tests): fixing import of datepicker on form and home component unit tests

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,4 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +12,11 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent],
+      imports: [HomeComponent, MatDatepickerModule, MatNativeDateModule],
+      providers: [
+        MatDatepickerModule,
+        importProvidersFrom([BrowserAnimationsModule]),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
--- src/app/shared/form/form.component.spec.ts
+++ src/app/shared/form/form.component.spec.ts
@@ -1,4 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormComponent } from './form.component';
 
@@ -8,7 +18,21 @@ describe('FormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FormComponent],
+      imports: [
+        FormComponent,
+        MatButtonToggleModule,
+        MatIconModule,
+        MatChipsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatButtonModule,
+      ],
+      providers: [
+        MatDatepickerModule,
+        importProvidersFrom([BrowserAnimationsModule]),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FormComponent);
