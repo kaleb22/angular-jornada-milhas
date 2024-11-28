14df70b2f309e4c98e124539243974320735302d
kaleb22 <kaleb22@hotmail.com>
2024-11-28 16:28:40 -0300

    fix(app-component): test - injecting provideRouter because of routerLink directive on the template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,21 @@
 import { TestBed } from '@angular/core/testing';
+import { provideRouter, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
 
 describe('AppComponent', () => {
+  const mockRoutes: Routes = [
+    {
+      path: 'login',
+      component: LoginComponent,
+    },
+  ];
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideRouter(mockRoutes)],
     }).compileComponents();
   });
 
