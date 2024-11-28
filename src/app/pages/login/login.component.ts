62942fe684f9ce45ab741c9515f2b1a3ed471c77
kaleb22 <kaleb22@hotmail.com>
2024-11-28 15:35:58 -0300

    feat(login-component): creating formGroup and binding it to template

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,16 @@
+import { Component, inject, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
-import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 import { BannerComponent } from '../../shared/banner/banner.component';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
@@ -19,7 +25,18 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password: FormControl;
+export class LoginComponent implements OnInit {
+  private formBuilder = inject(FormBuilder);
+  loginForm: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl(''),
+    });
+  }
+
+  login(): void {
+    console.log('logged successfully, ', this.loginForm.value);
+  }
 }
