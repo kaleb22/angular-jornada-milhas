3d0e91414e0238e2e76fcfb77d7107a85daabf0a
kaleb22 <kaleb22@hotmail.com>
2024-11-29 15:07:14 -0300

    feat(form-generic-component): creating a generic form to use in the sign up and edit pages

---
+++ src/app/shared/form-generic/form-generic.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormGenericComponent } from './form-generic.component';
+
+describe('FormGenericComponent', () => {
+  let component: FormGenericComponent;
+  let fixture: ComponentFixture<FormGenericComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FormGenericComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(FormGenericComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/form-generic/form-generic.component.ts
@@ -0,0 +1,74 @@
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio';
+
+import { CardComponent } from '../card/card.component';
+import { AutocompleteComponent } from '../autocomplete/autocomplete.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDividerModule } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-form-generic',
+  standalone: true,
+  imports: [
+    CardComponent,
+    MatCheckboxModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatRadioModule,
+    AutocompleteComponent,
+  ],
+  templateUrl: './form-generic.component.html',
+  styleUrl: './form-generic.component.scss',
+})
+export class FormGenericComponent implements OnInit {
+  formGeneric: FormGroup;
+  private formBuilder = inject(FormBuilder);
+  estado = new FormControl('', [Validators.required]);
+
+  ngOnInit(): void {
+    this.formGeneric = this.formBuilder.group({
+      nome: new FormControl('', [Validators.required]),
+      dataNascimento: new FormControl('', [Validators.required]),
+      genero: new FormControl('feminino', [Validators.required]),
+      cpf: new FormControl('', [Validators.required]),
+      telefone: new FormControl('', [Validators.required]),
+      cidade: new FormControl('', [Validators.required]),
+      estado: this.estado,
+      email: new FormControl('', [Validators.required, Validators.email]),
+      confirmaEmail: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ]),
+      senha: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      confirmaSenha: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+      aceitarTermos: new FormControl(false, [Validators.requiredTrue]),
+    });
+  }
+
+  createAccount(): void {
+    console.log('account successfully created');
+  }
+}
