6946f7b0261a935537f880f018bb012ae703d196
kaleb22 <kaleb22@hotmail.com>
2024-11-29 15:46:34 -0300

    feat(cadastro-component): creating structure of sign up page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'cadastro',
     pathMatch: 'full',
   },
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent,
   },
 ];
---
+++ src/app/pages/cadastro/cadastro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { BannerComponent } from '../../shared/banner/banner.component';
+import { FormGenericComponent } from '../../shared/form-generic/form-generic.component';
+
+@Component({
+  selector: 'app-cadastro',
+  standalone: true,
+  imports: [BannerComponent, FormGenericComponent],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.scss',
+})
+export class CadastroComponent {}
