6be7ae0320546d182220b6c4430f9ebd2c9c6174
kaleb22 <kaleb22@hotmail.com>
2024-12-06 11:05:11 -0300

    fix(tests): injecting httpClient and MatDateModule

--- src/app/core/services/passagens.service.spec.ts
+++ src/app/core/services/passagens.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { PassagensService } from './passagens.service';
 
@@ -6,7 +7,9 @@ describe('PassagensService', () => {
   let service: PassagensService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [provideHttpClient()],
+    });
     service = TestBed.inject(PassagensService);
   });
 
--- src/app/pages/busca/busca.component.spec.ts
+++ src/app/pages/busca/busca.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { BuscaComponent } from './busca.component';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
 
 describe('BuscaComponent', () => {
   let component: BuscaComponent;
@@ -8,7 +12,11 @@ describe('BuscaComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BuscaComponent],
+      imports: [BuscaComponent, MatNativeDateModule],
+      providers: [
+        provideHttpClient(),
+        importProvidersFrom([BrowserAnimationsModule]),
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(BuscaComponent);
