5fc59982b8908ba8a6ee3d6ec25de6b3d79158b3
kaleb22 <kaleb22@hotmail.com>
2024-12-09 17:58:39 -0300

    fix(passagem-filtro): test - mocking input property

--- src/app/shared/passagem-filtros/passagem-filtros.component.spec.ts
+++ src/app/shared/passagem-filtros/passagem-filtros.component.spec.ts
@@ -1,18 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PassagemFiltrosComponent } from './passagem-filtros.component';
+import { ResultadoBusca } from '../../core/types/types';
 
 describe('PassagemFiltrosComponent', () => {
   let component: PassagemFiltrosComponent;
   let fixture: ComponentFixture<PassagemFiltrosComponent>;
+  let mockResultado: ResultadoBusca;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [PassagemFiltrosComponent],
     }).compileComponents();
 
+    mockResultado = {
+      paginaAtual: 0,
+      ultimaPagina: 0,
+      total: 0,
+      precoMin: 0,
+      precoMax: 10,
+      resultado: [],
+    };
     fixture = TestBed.createComponent(PassagemFiltrosComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('resultadoBusca', mockResultado);
     fixture.detectChanges();
   });
 
