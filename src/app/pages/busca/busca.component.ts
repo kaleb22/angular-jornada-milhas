6be812957492083d0289d3509bfb101464a0f3f3
kaleb22 <kaleb22@hotmail.com>
2024-12-10 14:24:03 -0300

    feat(passagem-resumo): impl logic to generate tickets recommendation

--- src/app/pages/busca/busca.component.ts
+++ src/app/pages/busca/busca.component.ts
@@ -9,6 +9,8 @@ import { FormComponent } from '../../shared/form/form.component';
 import { PassagemInfoComponent } from '../../shared/passagem-info/passagem-info.component';
 import { FormService } from '../../core/services/form.service';
 import { PassagemFiltrosComponent } from '../../shared/passagem-filtros/passagem-filtros.component';
+import { PassagemResumoComponent } from '../../shared/passagem-resumo/passagem-resumo.component';
+import { Passagem, ResultadoBusca } from '../../core/types/types';
 
 @Component({
   selector: 'app-busca',
@@ -20,6 +22,7 @@ import { PassagemFiltrosComponent } from '../../shared/passagem-filtros/passagem
     CommonModule,
     PassagemInfoComponent,
     PassagemFiltrosComponent,
+    PassagemResumoComponent,
   ],
   templateUrl: './busca.component.html',
   styleUrl: './busca.component.scss',
@@ -28,16 +31,67 @@ export class BuscaComponent implements AfterViewInit {
   private passagensService = inject(PassagensService);
   private formService = inject(FormService);
 
+  ticketCheapest: Passagem;
+  ticketFatest: Passagem;
+  ticketRecommended: Passagem;
+  recommend: boolean = false;
+
   retornoBusca$ = this.passagensService.retornoBusca$.pipe(
-    tap((res) => console.log(res)),
+    tap((response) => {
+      if (this.checkRecommendation(response)) {
+        this.populateCheapestTicket(response);
+        this.populateFatestTicket(response);
+        this.populateRecommendedTicket(response);
+      }
+    }),
   );
+
+  private checkRecommendation(response: ResultadoBusca): boolean {
+    if (response.resultado.length >= 3) {
+      this.recommend = true;
+      return true;
+    }
+    this.recommend = false;
+    return false;
+  }
+
+  private populateCheapestTicket(response: ResultadoBusca): void {
+    this.ticketCheapest = response.resultado.reduce((prev, curr) => {
+      if (prev.total < curr.total) {
+        return prev;
+      }
+      return curr;
+    });
+  }
+
+  private populateFatestTicket(response: ResultadoBusca): void {
+    this.ticketFatest = response.resultado.reduce((prev, curr) => {
+      if (prev.tempoVoo < curr.tempoVoo) {
+        return prev;
+      }
+      return curr;
+    });
+  }
+
+  private populateRecommendedTicket(response: ResultadoBusca): void {
+    this.ticketRecommended = response.resultado.sort((a, b) => {
+      let first =
+        (a.tempoVoo / this.ticketCheapest.tempoVoo +
+          a.total / this.ticketCheapest.total) /
+        2;
+      let second =
+        (b.tempoVoo / this.ticketCheapest.total +
+          b.total / this.ticketCheapest.total) /
+        2;
+      return first - second;
+    })[0];
+  }
 
   ngAfterViewInit(): void {
     this.searchTickets();
   }
 
   searchTickets() {
-    console.log('searchTickets emmited');
     const searchData = this.formService.getSearchData();
     this.passagensService.search(searchData);
   }
--- src/app/shared/passagem-resumo/passagem-resumo.component.ts
+++ src/app/shared/passagem-resumo/passagem-resumo.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-passagem-resumo',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './passagem-resumo.component.html',
   styleUrl: './passagem-resumo.component.scss',
 })
-export class PassagemResumoComponent {}
+export class PassagemResumoComponent {
+  @Input() title: string;
+  @Input() value: number;
+  @Input() tripTime: number;
+  @Input() isRecommended: boolean;
+}
